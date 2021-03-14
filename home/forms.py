229ce6b7e86b06fb00e30bdc6a3db0148dfc9cef
sandy rosas <rosassandyboy@gmail.com>
2021-03-14 21:22:44 +0800

    include email field on sign up

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,19 @@ from django.core.validators import ValidationError
 from django import forms
 
 class SignUpForm(UserCreationForm):
-
+   email = forms.EmailField(required=True,label='Email',error_messages={'exists': 'Oops! Email already exist'})
    class Meta:
       model = User 
-      fields = ('username', 'password1', 'password2',)
+      fields = ('username', 'email', 'password1', 'password2',)
+
+   def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+   def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise ValidationError(self.fields['email'].error_messages['exists'])
+        return self.cleaned_data['email']
